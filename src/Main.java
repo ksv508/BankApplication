6a0a7430ef7405f7362b4e0b0d87cddd76b095d1
keshav <ksv508@gmail.com>
2023-06-10 00:32:01 +0530

    Both SBI and HDFC completed

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter BANK name in which you want to create account");
         String opt = sc.next();
-        if(opt.equals("HDFC")) {
+        if(opt.equals("HDFC") || opt.equals("hdfc")) {
             System.out.println("Welcome To HDFC");
 
             System.out.println("Enter Customer Name");
@@ -20,7 +20,7 @@ public class Main {
 
             while(true) {
                 System.out.println("Choose the service you want to opt");
-                System.out.println("1. Create an Bank Account");
+                System.out.println("1. Create a new Bank Account");
                 System.out.println("2. Check Balance");
                 System.out.println("3. Add Money to your Account");
                 System.out.println("4. Withdraw Money From Your Account");
@@ -74,8 +74,74 @@ public class Main {
                 }
             }
         }
+        else if(opt.equals("SBI") || opt.equals("sbi")){
+            System.out.println("Welcome To State Bank Of India");
+
+            System.out.println("Enter Customer Name");
+            String name = sc.next();
+            System.out.println("Enter Customer Starting balance");
+            double balance = sc.nextDouble();
+            System.out.println("Enter Password");
+            String password = sc.next();
+            SBIAccount sbiAccount = new SBIAccount(name, balance, password);
+            while(true) {
+                System.out.println("Choose the service you want to opt");
+                System.out.println("1. Create a new Bank Account");
+                System.out.println("2. Check Balance");
+                System.out.println("3. Add Money to your Account");
+                System.out.println("4. Withdraw Money From Your Account");
+                System.out.println("5. Change Bank Password");
+                System.out.println("6. Calculate Interest");
+                System.out.println("7. Exit");
+                int ser = sc.nextInt();
+                switch (ser) {
+                    case 1:
+                        System.out.println("Enter Customer Name");
+                        name = sc.next();
+                        System.out.println("Enter Customer Starting balance");
+                        balance = sc.nextDouble();
+                        System.out.println("Enter Password");
+                        password = sc.next();
+                        sbiAccount = new SBIAccount(name, balance, password);
+                        System.out.println("Congrats " + sbiAccount.getName() + ". Your account has been created with account number " + sbiAccount.getAccountNo());
+                        break;
+                    case 2:
+                        System.out.println("Enter your password");
+                        password = sc.next();
+                        System.out.println(sbiAccount.fetchBalance(password));
+                        break;
+                    case 3:
+                        System.out.println("Enter Amount you want to deposit");
+                        double amount = sc.nextDouble();
+                        sbiAccount.addMoney(amount);
+                        break;
+                    case 4:
+                        System.out.println("Enter Amount you want to Withdraw");
+                        amount = sc.nextDouble();
+                        System.out.println("Enter Your Password");
+                        password = sc.next();
+                        System.out.println(sbiAccount.withdrawMoney(password, amount));
+                        break;
+                    case 5:
+                        System.out.println("Enter your oldpassword");
+                        String oldpassword = sc.next();
+                        System.out.println("Enter your newpassword");
+                        String newpassword = sc.next();
+                        System.out.println(sbiAccount.changePassword(newpassword, oldpassword));
+                        break;
+                    case 6:
+                        System.out.println("Enter the number of years to calculate interest");
+                        int years = sc.nextInt();
+                        System.out.println("Interest for " + years + " years is :" + sbiAccount.calculateInterest(years));
+                        break;
+                    case 7:
+                        System.out.println("Thank you for banking with us ");
+                        exit(0);
+                }
+            }
+        }
         else{
-            System.out.println("you are in SBI");
+            System.out.println("Please choose valid Bank");
         }
     }
 }
--- src/SBIAccount.java
+++ src/SBIAccount.java
@@ -1,28 +1,97 @@
+import java.util.UUID;
+
 public class SBIAccount implements BankAccountInterface{
     private String name;
+    private String accountNo; // random and unique --> UUID
+    private double balance;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAccountNo() {
+        return accountNo;
+    }
+
+    public void setAccountNo(String accountNo) {
+        this.accountNo = accountNo;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public double getRateOfInterest() {
+        return rateOfInterest;
+    }
+
+    final double rateOfInterest = 6.1;
+    // without parameters constructors
+    public SBIAccount(){
+    }
+    // with parameters constructors
+    public SBIAccount(String name, double balance, String password) {
+        this.name = name;
+        this.accountNo = String.valueOf(UUID.randomUUID()); // universally unique identifier (UUID)
+        this.balance = balance;
+        this.password = password;
+    }
 
     @Override
     public String fetchBalance(String password) {
-        return null;
+        if(this.password.equals(password)){
+            return "Your current Balance is : " + this.balance;
+        }
+        return "Incorrect password";
     }
 
     @Override
     public double addMoney(double amount) {
-        return 0;
+        this.balance += amount;
+        return balance;
     }
 
     @Override
     public String withdrawMoney(String password, double amount) {
-        return null;
+        if(this.password.equals(password)){
+            if(this.balance >= amount) {
+                this.balance -= amount;
+                return "Money has been withdrawn. The remaining balance is :" + this.balance;
+            }
+            else
+                return "Insufficient balence";
+        }
+        return "Incorrect Password";
     }
 
     @Override
     public String changePassword(String newPassword, String oldPassWord) {
-        return null;
+        if(this.password.equals(oldPassWord)){
+            this.password = newPassword;
+            return "Your Password is Successfully changed";
+        }
+        return "Wrong Password";
     }
 
     @Override
     public double calculateInterest(int years) {
-        return 0;
+        return (this.balance * this.rateOfInterest * years)/100;
     }
 }
